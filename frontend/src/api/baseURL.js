2a27a3691482bc04a118a4885b0daccc34cbc20e
mz7701 <korean2071@naver.com>
2025-11-18 12:39:56 +0900

    railway v5

--- frontend/src/api/baseURL.js
+++ frontend/src/api/baseURL.js
@@ -1,4 +1,4 @@
 // src/api/baseURL.js
 
 // 🔥 나중에 Render에 백엔드 배포하고 나면 그 URL로 아래 값만 바꾸면 됨
-export const API_BASE_URL = "https://springboot-react-kakaoboard.onrender.com";
+export const API_BASE_URL = "https://springboot-react-kakaoboard-production.up.railway.app";
