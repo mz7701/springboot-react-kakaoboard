a296df299b114857f41a663dfe61e343b545576d
mz7701 <korean2071@naver.com>
2025-11-28 17:01:15 +0900

    japen version2

--- frontend/src/api/baseURL.js
+++ frontend/src/api/baseURL.js
@@ -2,6 +2,6 @@
 
 // 🔥 後で Render にバックエンドをデプロイしたら、
 //    その URL に合わせて下の値だけ変更すればOKです。
-export const API_BASE_URL = "http://192.168.0.40:8080";
+//export const API_BASE_URL = "http://192.168.0.40:8080";
 
-// export const API_BASE_URL = "https://springboot-react-kakaoboard.onrender.com";
+ export const API_BASE_URL = "https://springboot-react-kakaoboard.onrender.com";
